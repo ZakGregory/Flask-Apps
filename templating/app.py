76fd7719e190f3ca34ce5f019ce30086f3a2634d
ZakGregory <zgregoryk@gmail.com>
2020-11-03 11:52:14 +0000

    Added names exercise to templating

--- templating/app.py
+++ templating/app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/zak')
-def ben():
+def zak():
     return render_template('zak.html')
 
 @app.route('/jeff')
-def harry():
+def jeff():
     return render_template('jeff.html')
+
+@app.route('/exercise')
+def exercise():
+    return render_template('exercise.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
