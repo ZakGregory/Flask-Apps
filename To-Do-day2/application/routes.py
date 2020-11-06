17db87e2600ead4ef4418dfdd2a71f38b79d5041
ZakGregory <zgregoryk@gmail.com>
2020-11-06 15:45:36 +0000

    Added features from days 3+4+%

--- To-Do-day2/application/routes.py
+++ To-Do-day2/application/routes.py
@@ -1,12 +1,23 @@
 from flask import render_template, redirect, request, url_for
 from application import app, db
 from application.models import Tasks
-from application.forms import InputForm 
+from application.forms import InputForm
+from sqlalchemy import asc, desc
 
 @app.route('/')
 @app.route('/home')
-def home():
-    all_tasks=Tasks.query.all()
+@app.route('/home/<method>')
+def home(method=""):
+    if method=="O-N":
+        all_tasks=Tasks.query.filter().order_by(Tasks.id)
+    elif method=="N-O":
+        all_tasks=Tasks.query.filter().order_by(desc(Tasks.id))
+    elif method=="CF":
+        all_tasks=Tasks.query.filter().order_by(desc(Tasks.complete))
+    elif method=="NCF":
+        all_tasks=Tasks.query.filter().order_by(Tasks.complete)
+    else:
+        all_tasks=Tasks.query.all()
     return render_template('home.html', all_tasks=all_tasks)
 
 @app.route('/add', methods=['GET','POST'])
@@ -34,14 +45,22 @@ def incomplete(taskid):
     task_to_update.complete = False
     db.session.commit()
     return redirect(url_for('home'))
-''' 
-@app.route('/update')
-def update():
-''' 
+ 
+@app.route('/update/<taskid>', methods=['GET','POST'])
+def update(taskid):
+    form = InputForm()
+    task_to_update = Tasks.query.get(taskid)
+    
+    if form.validate_on_submit():
+        task_to_update.name = form.new_task.data        
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
+ 
 
-@app.route('/delete')
-def delete():
-    task_to_delete = Tasks.query.first()
-    db.sessiom.delete(task_to_delete)
+@app.route('/delete/<taskid>')
+def delete(taskid):
+    task_to_delete = Tasks.query.get(taskid)
+    db.session.delete(task_to_delete)
     db.session.commit()
     return redirect(url_for('home'))
